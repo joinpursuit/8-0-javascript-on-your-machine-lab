b157ed5785b981d46bf822ae3a4e9364abec28ba
Rohan-Sterling <rohansterling@pursuit.org>
2021-07-16 17:22:26 -0400

    add a calulator function w/adding & subtracting functionality

--- index.js
+++ index.js
@@ -4,7 +4,51 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    const operator = process.argv[2];
+    const inputs = process.argv.slice(3);
+    let number = 0;
+
+    if(!operator) {
+        return 'No operation provided...'
+    }
+    
+    if(operator === 'plus' && inputs.length) {
+        for(let i = 0; i < inputs.length; i++) {
+            
+            number += Number(inputs[i]);
+        }
+        return number;
+
+
+    } else if (operator === 'minus' && inputs.length) {
+        number = Number(inputs[0]);
+        for(let i = 1; i < inputs.length; i++) {
+            
+           number -= Number(inputs[i]);
+
+        }
+        return number;
+
+    } else if (operator === 'divide') {
+        return 'Invalid operation: divide'
+
+    } else if (operator === 'multiply') {
+        return 'Invalid operation: multiply'
+
+    } else if (operator === 'modulo') {
+        return 'Invalid operation: modulo'
+
+    } else {
+        return 'No numbers provided...'
+    }
+
+    return 'done'
+}
+
+
+
+
 
 // Don't change anything below this line.
 module.exports = calculator;

395bf35348924d1dc4417f8bd21bfd5c693e7e7b
Theodore Frazier <theo.frazier@icloud.com>
2021-07-16 16:40:03 -0400

    wminus and plus working

--- index.js
+++ index.js
@@ -4,7 +4,42 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+
+    let inputArr = process.argv;
+    let operation = inputArr[2];
+    let numbers = inputArr.slice(3);
+    let newNum = 0;
+
+
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        let num = Number(number);
+
+
+        if (operation === "plus") {
+            newNum += num;
+        } else if (operation === "minus") {
+            if (i === 0) {
+                newNum += num;
+            } else {
+                newNum -= num;
+            }
+        }
+        /*if (operation !== 'plus' || operation !== 'minus') {
+            newNum = "no operation provided...";
+        } else if (!numbers) {
+            newNum = "No numbers provided...";
+        }
+        */
+
+
+    }
+    return newNum;
+}
+
+
+
 
 // Don't change anything below this line.
 module.exports = calculator;

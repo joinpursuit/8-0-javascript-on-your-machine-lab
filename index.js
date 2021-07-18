5a782197930a7162e09792de53e0cf94a52cfb1e
salrey <tekdehn@gmail.com>
2021-07-17 20:52:46 -0400

    Add Solution to Calculator Exercise

--- index.js
+++ index.js
@@ -4,7 +4,35 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+
+
+function calculator() {
+const operator = process.argv[2]
+const numArray = process.argv.slice(3)
+const num = numArray.map((element) => Number(element))
+
+if (!operator) {
+    return "No operation provided...";
+};
+
+if (operator !== 'plus' && operator !== 'minus') {
+    return input = `Invalid operation: ${operator}`;
+} else {
+    for (let i of num) {
+        if (!i) { 
+            return "No numbers provided...";
+        } else if (operator === 'plus') {
+            const add = (accumulator, i) => accumulator + i;
+            return num.reduce(add);
+        } else if (operator === 'minus') {
+            const subtract = (accumulator, i) => accumulator - i;
+            return num.reduce(subtract);
+            } return "No numbers provided..."
+        }
+        return "No numbers provided..." 
+    }
+}
+
 
 // Don't change anything below this line.
 module.exports = calculator;

d4eab2460d34ab536b036092e92eb2f8a44bb93f
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-07-19 00:25:13 -0400

    test.JS function calculator using process.argv

--- index.js
+++ index.js
@@ -4,7 +4,29 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+    function calculator() {
+        let inputArr = process.argv; 
+        let operation = inputArr[2];      
+        let numbers = inputArr.slice(3);   
+        let total = Number(numbers[0]);
+        if (!operation) {
+            return "No operation provided..."
+        }else if (numbers.length === 0) {
+            return "No numbers provided..."
+        }else if(operation === "plus" || operation === "minus") {
+            for (let i = 1; i < numbers.length; i++) {
+                if (operation === "plus") {   
+                    total += Number(numbers[i]);  
+                } else if (operation === "minus") {
+                    total -= Number(numbers[i]);
+                } 
+            }     
+            return total;
+        }    
+        else {
+            return "Invalid operation: " + operation;  
+        }
+    }
 
 // Don't change anything below this line.
 module.exports = calculator;

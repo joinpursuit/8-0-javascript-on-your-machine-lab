ec03a39708d173960ad26c05a416375185bdd31b
rob <robwalker@pursuit.org>
2021-07-19 19:08:52 -0400

    first push

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 /**
  * The function below has no parameters. Instead, access the arguments from the command line.
- * The first argument passed after the filename should be either "plus" or "minus", which represents the kind of calculation that will be done. Every argument afterwards should be a number.
+ * The first argument passed after the filename should be either "plus" or "minus", \n
+ * which represents the kind of calculation that will be done. Every argument afterwards should be a number.
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
- * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
+ * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments \n
+ * added to the command line.
  */
-function calculator() {}
+function calculator() {
+    let inputArr = process.argv;
+    let operation = inputArr[2];
+    let numbers = inputArr.slice(3);
+    let total = Number(numbers[0]);
+    //console.log(numbers);
+    if(!operation) {
+        return 'No operation provided...';
+      } else if (numbers.length === 0) {
+    return'No numbers provided...'; 
+      } else if (operation === 'plus' || operation === 'minus') {
+    for (let i = 1; i < numbers.length; i++) {
+        
+       if (operation === 'plus') {
+           total += Number(numbers[i]);
+       } else if (operation === 'minus') {
+           total -= Number(numbers[i]);   
+        } 
+    }
+    return total;
+} else {
+    return 'Invalid operation: ' + operation;
+}
+}
+//console.log(process.argv[2]);
 
 // Don't change anything below this line.
 module.exports = calculator;

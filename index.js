69a24ebb2489d4a706ad3a749d3180e0e70f24b7
Angie G <angieg@Angies-MBP.fios-router.home>
2021-07-19 00:47:21 -0400

    JavaScript on your Machine Lab

--- index.js
+++ index.js
@@ -4,7 +4,47 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+ function calculator() {
+    // Create variables for clarity 
+    let operation = process.argv[2];
+    let numbers = process.argv.slice(3);
+    // Let subTotal start as the first number. 
+    let subTotal = Number(numbers[0]);
+    // Edge cases: if & else statement 
+    // If operation is missing, return error.
+    if (operation === undefined) {
+        return `No operation provided...`;
+    }
+    // If numbers are not provided and array is empty, return error.
+    else if (numbers[0] === undefined) {
+        return `No numbers provided...`;
+    }
+    // If operation is not plus or minus, return error message.
+    else if (operation !== 'plus' && operation !== 'minus') {
+        return `Invalid operation: ${operation}`;
+    } 
+    // Number calculations: 2 loops through numbers.
+    // If operation is minus, then subtract the numbers. 
+    if (operation === 'minus') {
+        for (i = 1; i < numbers.length; i++) {
+            subTotal -= Number(numbers[i]);
+        }
+    
+    }
+    // If operation is plus, then add the numbers.
+    if (operation === 'plus') {
+        for (i = 1; i < numbers.length; i++) {
+            subTotal += Number(numbers[i]);
+        }
+        
+    }
+    // Return calculated number 
+    // After calculations, declare total and return it. 
+    let total = subTotal 
+    return total;
+}
+
+
 
 // Don't change anything below this line.
 module.exports = calculator;

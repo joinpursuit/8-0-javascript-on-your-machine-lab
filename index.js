a0a48682c1be5078aac7dd03285060c0cb876d59
justin <justin.d.diaz@gmail.com>
2021-07-19 00:41:18 -0400

    answer'

--- index.js
+++ index.js
@@ -4,33 +4,42 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {
-    let slicArray= [];
-    process.argv.push(command, ...numbers);
-    slicArray = process.argv.slice(3)
-    slicArray = slicArray.map(Number)
-    for (let i = 0; i < slicArray.length; i++) {
-        //Adding multiple numbers (-+)
-        if (command == 'plus') {
-             outPut = slicArray.reduce(function(a, b){
-                 return a + b;
-              }, 0);
+ function calculator() {
+    //This slices part of array process.argv from index 3
+       let slicArray = process.argv.slice(3);
+       //Creating variable to return result in if statement
+       let outPut = 0;
+       //converts slicArray elements in string into number
+       slicArray = slicArray.map(Number)
+       //checks if no input is provided
+       if (!process.argv[2]) {
+           return "No operation provided...";
+      }
+      //checks if no number is provided
+      else if(!slicArray.length){
+           return "No numbers provided...";
+      }
+      //Iterates through the slicArray from process.argv array
+      for (let i = 0; i < slicArray.length; i++) {
+       //checks if Input is 'modulo' and returns error message
+       if (process.argv[2] === 'modulo') {
+           return "Invalid operation: modulo"
+      }
+          //Checks if  inputs are plus and a number
+       else if (process.argv[2] === "plus") {
+           //adds elements in the array slicArray (-+)
+               const reducer = (accumulator, currentValue) => accumulator + currentValue;
+               outPut = slicArray.reduce(reducer)
+       }
+        ////Checks if  inputs are minus and a number
+       else if (process.argv[2] === "minus") {
+           //Subtracts elements  in the array slicArray (-+)
+           const reducer = (accumulator, currentValue) => accumulator - currentValue;
+           outPut = slicArray.reduce(reducer)
+           }
         }
-        //Subtracting two/multiple numbers (-+)
-        else  if (command == 'minus') {
-            outPut = slicArray.reduce(function(a, b){
-                return a - b;
-              }, 0);
-        } else if (command === 'modulo'){
-        return "Invalid operation: modulo";
-    } else if (command === undefind) {
-        return "No numbers provided...";
-    } else {
-        return "No operation provided..."
-    }
-    return outPut; 
-    }
-}
+        return outPut;
+       }
 
 // Don't change anything below this line.
 module.exports = calculator;

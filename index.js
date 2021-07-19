81c58c61cfc0736c7dd9238401612025c779fa9e
Laiba Sajid <sajid.laiba378@gmail.com>
2021-07-19 18:46:24 -0400

    Laiba Sajid, James Russel Javascript on your machine lab- with tests
    :q
    
    :

--- index.js
+++ index.js
@@ -6,25 +6,27 @@
  */
 function calculator(){
     let inputArr = process.argv;
-    let operation = inputArr[2];
-    let numbers = inputArr.slice(3);
-    let result = 0 ;
-        
-        if(!operation){
-            console.log("No operation provided...");
-        }else if(!numbers){
-            console.log("No numbers provided...");
-        }else if(operation === "plus"){
-          for (let i = 0; i < numbers.length; i++){
-            result += parseInt(numbers[i]);
-          }
-        }else if(operation === "minus"){
-            result = numbers[0];
-            for (let i = 1; i < numbers.length; i++){
-             result -= parseInt(numbers[i]); 
-            }
-        }
-    console.log(result);
+  let operation = inputArr[2];
+  let numbers = inputArr.slice(3);
+  let result = 0;
+  if (!operation) {
+    return "No operation provided...";
+  } else if (!numbers.length) {
+    return "No numbers provided...";
+  } else if (operation === "plus") {
+    for (let i = 0; i < numbers.length; i++) {
+      result += Number(numbers[i]);
+    }
+  } else if (operation === "minus") {
+    result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+      result -= Number(numbers[i]);
+    }
+    } else {
+        return `Invalid operation: ${operation}`
+    }
+  // console.log(numbers)
+  return result;
 }
 
 // Don't change anything below this line.

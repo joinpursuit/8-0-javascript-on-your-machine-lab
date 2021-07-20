67f4785cdf7a8bd469f8c0cc64df8b36c4a4906a
Nima sherpa <Toplouie78@gmail.com>
2021-07-19 23:52:36 -0400

    jsMachineLab

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ const { run } = require("jest");
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {
-   let inputArr = process.argv;
+
+function calculator(x,y) {
+   let  inputArr = process.argv;
    let operation = inputArr[2];
    let numbers = inputArr.slice(3)
-   return numbers;
-}
 
+   if(numbers === "divide" ){
+      return "Invalid operation: divide";
+   }else if (operation === "plus"){
+      return numbers + numbers ;
+   }else if (operation === ""){
+      return "No numbers provided...";
+   }else {
+       return "No operation provided...";
+   }
+   
+}
 
 
 

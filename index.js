8a7a0a376ba4e53ac953f04376826997dd0fe6ea
CarlosAMoran <Carlosamoran18@gmail.com>
2021-07-19 21:18:46 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 /**
  * The function below has no parameters. Instead, access the arguments from the command line.
- * The first argument passed after the filename should be either "plus" or "minus", which represents the kind of calculation that will be done. Every argument afterwards should be a number.
+ * The first argument passed after the filename should be either "plus" or "minus", 
+ * which represents the kind of calculation that will be done. 
+ * Every argument afterwards should be a number.
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
- * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
+ * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments 
+ * added to the command line.
  */
-function calculator() {}
 
-// Don't change anything below this line.
+function calculator() {
+    let inputArr = process.argv;
+    console.log (process.argv)
+    let operation = inputArr[2];
+    console.log(operation)
+    let numbers = inputArr.slice(3);
+    console.log(numbers)
+    let num = numbers.map(Number)
+    console.log("num: " + (num))
+    let reduced = Math.abs(num.reduce((a,b)=> a + b,0));
+    console.log (reduced)
+    if (operation === undefined){
+        console.log("operation undefined trigged")
+        return ("No operation provided...");
+    }
+    else if (isNaN(reduced)=== true || reduced === 0){
+        console.log("isNaN trigged")
+        return ("No numbers provided...");
+    }
+    else if (operation !== '+'  && operation !== 'plus' && operation !== '-' && operation !== 'minus')
+    {
+        console.log("Invalid operation triggered")
+        return (`Invalid operation: ${operation}`);
+    } 
+     else if (operation == '+'  || operation == 'plus'){
+        var result =  (num.reduce((a,b)=> a + b));
+    }
+      else if (operation == '-'|| operation == 'minus'){
+        
+        var result = (num.reduce((a,b)=> a - b));
+      }
+      return result;
+    }
+    
+      
+    
+ 
+//Don't change anything below this line.
 module.exports = calculator;

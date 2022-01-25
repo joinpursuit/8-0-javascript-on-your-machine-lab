b5d36b52555fdad89caf181a14bc570e9a448954
Krystal Mansour <isonech@krystals-air.myfiosgateway.com>
2022-01-25 05:06:34 -0500

    on yourway to success

--- index.js
+++ index.js
@@ -4,7 +4,74 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    let input = process.argv[2];
+      
+    
+      if (process.argv.length <= 2) {
+        return 'No operation provided...';
+
+    } else if (process.argv.length <= 3) {
+        return 'No numbers provided...'
+
+    } else if (input === 'modulo') {
+        return 'Invalid operation: modulo';
+
+    } else if (input === 'plus') {
+        let num = 0;
+        for (let i = 3; i < process.argv.length; i++) {
+         num += Number(process.argv[i]);
+          return num; 
+}       
+        
+    } else if (process.argv === 'minus') {
+        let diff = (process.argv[3]);
+        
+        for (let i = 4; i < process.argv.length; i--) {
+           diff -= Number(process.argv[i]);
+             return diff;
+        }
+        
+    
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Don't change anything below this line.
 module.exports = calculator;

fd26bc104c5590902e7505bf704e70d03e230e6c
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2022-01-25 13:11:43 -0500

    declare the num variable and returns num for sums and differences

--- index.js
+++ index.js
@@ -4,7 +4,12 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+  let num
+    
+return num;
+
+}
 
 // Don't change anything below this line.
 module.exports = calculator;

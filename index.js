cf97cee0143dc23442a43e1502405570662f0c39
Joel Guillen <joelguillen@Joels-Air.fios-router.home>
2022-01-26 15:18:56 -0500

    iteration

--- index.js
+++ index.js
@@ -5,17 +5,31 @@
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
  function calculator() {
-    if(process.argv[2] === "plus") {
-         let answer = Number(process.argv[3]) + Number(process.argv[4]);
-         return answer
-     } else if (process.argv[2] === "minus") {
-         let answer = Number(process.argv[3]) - Number(process.argv[4]);
-         return answer
-     } else {
-         let opInQuestion = String(process.argv[2])
-         let illegalOpMsg = "Illegal Operation: " + opInQuestion
-         return illegalOpMsg
-     }
+// plus 1 + 2 + 3 = 6        
+// process.argv[2] + process.argv[3] + process.argv[4]
+// count how many numbers there are
+// process.argv.length - 2
+// isolate the numbers from the worded arguments
+// add them
+// process.argv[3] + process.argv[4] + process.argv[5] + process.argv[6] + process.argv[ETC];
+let solution = process.argv[3];
+
+if(process.argv[2] === "plus") { 
+            // Find starting point, how long you want to loop, keep track; "node run.js plus 1 2 3" Length of 6; 
+            for(let i = 4; i < process.argv.length; i++) {
+                solution = Number(solution) + Number(process.argv[i]);
+            }
+            return solution
+        } else if (process.argv[2] === "minus") {
+            for(let i = 4; i < process.argv.length; i++) {
+                solution = Number(solution) - Number (process.argv[i]);
+            }
+            return solution
+        } else {
+            let opInQuestion = String(process.argv[2])
+            let illegalOpMsg = "Illegal Operation: " + opInQuestion
+            return illegalOpMsg
+    }
 }
 
 // Don't change anything below this line.

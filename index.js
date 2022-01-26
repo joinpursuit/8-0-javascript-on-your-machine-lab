4834f8d109ea630eee08c63f9d8c256fd1e1cf59
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-01-25 19:28:41 -0500

    final edits

--- index.js
+++ index.js
@@ -13,30 +13,50 @@ function calculator(command, num1, num2) {
     command = process.argv[2];
     let start = Number(process.argv[3]);
     
-
-    if (command === 'plus') {
-        for (let i = 4; i < process.argv.length ; i++) {
-            start += Number(process.argv[i]);
-        }
-        return start; // works for adding two numbers w/ command = plus
-    
-    } else if (command === 'minus') {
-        for (let i = 4; i < process.argv.length ; i++) {
-            start -= Number(process.argv[i]);
-        }
-        return start; // works for subtracting any amount of #'s w/command = minus
-    
-    } else if (isNaN(command)) {
-        if (command !== 'minus' && command !== 'plus') {
+    if (isNaN(command)){
+        if (command === 'plus') {
+            for (let i = 4; i < process.argv.length ; i++) {
+                start += Number(process.argv[i]);
+            }
+            return start; // works for adding two numbers w/ command = plus
+        
+        } else if (command === 'minus') {
+            for (let i = 4; i < process.argv.length ; i++) {
+                start -= Number(process.argv[i]);
+            }
+            return start; // works for subtracting any amount of #'s w/command = minus
+            
+        } else if (command === 'plus' || command === 'minus' && start === NaN){
+            return "No numbers provided..." ;
+        } else {
             return `Invalid operation: ${command}` ;
-        } else if (!isNaN(command)) {
-            return "No operation provided..." ;
-        } else {}
-        return "No numbers provided..." ;
-
-    } else {
-        console.log(process.argv);
+        }
+        
+    } else if (!isNaN(command)) {
+        return "No operation provided..." ;
     }
+
+
+    // if (command === 'plus') {
+    //     for (let i = 4; i < process.argv.length ; i++) {
+    //         start += Number(process.argv[i]);
+    //     }
+    //     return start; // works for adding two numbers w/ command = plus
+    
+    // } else if (command === 'minus') {
+    //     for (let i = 4; i < process.argv.length ; i++) {
+    //         start -= Number(process.argv[i]);
+    //     }
+    //     return start; // works for subtracting any amount of #'s w/command = minus
+    
+    // } else if (isNaN(command)) {
+    //     if (command !== 'minus' && command !== 'plus') {
+    //         return `Invalid operation: ${command}` ;
+    //     } else if (!isNaN(command)) {
+    //         return "No operation provided..." ;
+    //     } else {}
+    //     return "No numbers provided..." ;
+
         
 
 

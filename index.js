d629df8da0b989cba03ed9ce6df631cfe5846a14
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2022-01-26 00:28:35 -0500

    finalize index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /**
  * The function below has no parameters. Instead, access the arguments from the command line.
  * The first argument passed after the filename should be either "plus" or "minus", which represents the kind of calculation that will be done. Every argument afterwards should be a number.
@@ -5,10 +6,27 @@
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
 function calculator() {
-  let num
-    
-return num;
+ const command = process.argv[2];
+ let num = Number(process.argv[3]);
+ let numbers = process.argv.slice(4)
+    if(!command ){
+      return  "No operation provided..."
+    }
+    if(!numbers.length){
+      return "No numbers provided..."
+    }
+    if(command !== 'plus' && command !== 'minus'){
+      return  `Invalid operation: ${command}`
+    }
+  for (let number of numbers) {
+    if (command === 'plus') {
 
+      num += Number(number);
+    } else {
+      num -= Number(number);
+    }
+  }
+return num 
 }
 
 // Don't change anything below this line.

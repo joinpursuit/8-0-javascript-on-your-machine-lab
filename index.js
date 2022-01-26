cf3196c92db95ab4e0c55c4a295bada2ecc9fa6b
Joel Guillen <joelguillen@Joels-Air.fios-router.home>
2022-01-25 22:09:37 -0500

    basic calc function

--- index.js
+++ index.js
@@ -4,7 +4,19 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+ function calculator() {
+    if(process.argv[2] === "plus") {
+         let answer = Number(process.argv[3]) + Number(process.argv[4]);
+         return answer
+     } else if (process.argv[2] === "minus") {
+         let answer = Number(process.argv[3]) - Number(process.argv[4]);
+         return answer
+     } else {
+         let opInQuestion = String(process.argv[2])
+         let illegalOpMsg = "Illegal Operation: " + opInQuestion
+         return illegalOpMsg
+     }
+}
 
 // Don't change anything below this line.
 module.exports = calculator;

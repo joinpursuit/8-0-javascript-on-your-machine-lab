63b0367d9b8bfb5dc9fcaf111251628c300d489e
Zane <zaneahmed4@gmail.com>
2022-01-25 19:06:11 -0500

    lab

--- index.js
+++ index.js
@@ -4,7 +4,27 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    const operator = process.argv[2]
+    const arr = process.argv.slice(3)
+    for (let i = 0; i < arr.length; i++){
+        let sum = 0
+        let diff = 0
+        let num = parseInt(arr[i])
+     if(operator === "plus") {
+        return  sum += num
+    } else if(operator === "minus") {
+        return diff -= num
+    } else if(operator !== "plus" || "minus"){
+        return "Invalid operation: " + operator
+    } else if (operator === null){
+        return `No operation provided...`
+    } else if (num === null){
+        return `No numbers provided...`
+}
+}
+}
+calculator();
 
 // Don't change anything below this line.
 module.exports = calculator;

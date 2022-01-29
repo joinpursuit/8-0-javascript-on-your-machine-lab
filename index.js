19cc06c8506170dcf246f51a11f4cb677fc66e03
masmei <masonmei@pursuit.org>
2022-01-28 20:02:13 -0600

    finished team lab

--- index.js
+++ index.js
@@ -5,28 +5,35 @@
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
  function calculator() {
-    const input = process.argv;
-    const operator = process.argv[2];
-    const numArr = input.slice(3);
-    let result = Number(numArr[0]);
-    if(!operator) {
-        result = "No operation provided...";
-    }else if (numArr.length === 0) {
-        result = "No numbers provided...";
-    } else if (operator === "plus") {
-        for(let i = 1; i < numArr.length; i++){
-            result += Number(numArr[i]);
-        }    
-    }else if (operator === "minus") {
-        for(let i = 1; i < numArr.length; i++){
-            result -= Number(numArr[i]);
-        }
-    }else if (operator !== "plus" && operator !== "minus") {
-        result = `Invalid operation: ${operator}`;
+    let operation = process.argv[2];
+    let num = process.argv[3];
+    if(!operation){
+       return "No operation provided...";
     }
+    
+    if(process.argv.length < 4) {
+    return "No numbers provided...";
+    }
+    
    
-     return result;
+     //__________________________
+     if(operation === 'plus'){
+        let sum = 0
+       for(let i = 3; i < process.argv.length; i++){
+     sum += Number(process.argv[i]);
+       }
+       return sum
+     }
+     if(operation === "minus") {
+        let diff = process.argv[3]
+        for(let i = 4; i < process.argv.length; i++){
+         diff -= Number(process.argv[i]);
+       }
+       return diff
+     }
+     if(operation !== 'plus' && operation !== 'minus'){
+        return `Invalid operation: ${operation}`
+        }
  }
-
 // Don't change anything below this line.
 module.exports = calculator;

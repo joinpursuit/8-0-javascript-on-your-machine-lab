40c0ed1835ff58447fb366d32976052913cd3d1c
KamNotCam <Kedari.a.matthews@gmail.com>
2022-02-04 17:18:35 -0500

    Finished

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 /**
- * The function below has no parameters. Instead, access the arguments from the command line.
- * The first argument passed after the filename should be either "plus" or "minus", which represents the kind of calculation that will be done. Every argument afterwards should be a number.
- * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
- * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
+ * The function below has no parameters. Instead, 
+ * access the arguments from the command line.
+ * The first argument passed after the filename
+ *  should be either "plus" or "minus", which 
+ * represents the kind of calculation that will 
+ * be done. Every argument afterwards should be 
+ * a number.
+ * Depending on the operation, either add up all
+ * of the numbers or subtract all of the numbers,
+ * from left to right.
+ * @returns {number} The result of either adding
+ * all numbers or subtracting all numbers, 
+ * depending on the arguments added to the 
+ * command line.
  */
-function calculator() {}
+function calculator() {
+    let result = 0;
+if(process.argv.length < 3) {
+    return `No operation provided...`
+} 
+if(process.argv[2] === 'plus' || process.argv[2] === 'minus'){
+ if(process.argv.length <= 3){
+    return `No numbers provided...`;
+ }
+} 
+if(process.argv[2] === 'plus'){
+  for (let i = 3; i < process.argv.length; i++) {
+    result += Number(process.argv[i])
+  } return result;
+ }
+if(process.argv[2] === 'minus') {
+  result = process.argv[3]
+ for (let i = 4; i < process.argv.length; i++) {
+  result -= Number(process.argv[i])
+  } return result;
+ } 
 
+else { result = `Invalid operation: ${process.argv[2]}`
+    return result;
+ }
+ return result;
+} 
+calculator();
 // Don't change anything below this line.
 module.exports = calculator;

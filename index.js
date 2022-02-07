c02f8c0f4b2d337d5e9829a66f8ddb45f4a5527c
Samantha Aviles <samanthaaviles85@gmail.com>
2022-02-06 22:47:40 -0500

    machine lab

--- index.js
+++ index.js
@@ -4,29 +4,35 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator(number) {
-    let input = process.argv;
-    let operator = process.argv[2];
-    let numArr = input.slice(3);
-    let result = Number(numArr[0]);
-        if (!operator) {
-            result = "No operation provided...";
-            
-        } else if (operator === "plus") {
-            for (let i = 1; i < numArr.length; i++) {
-                result += Number(numArr[i]);
-            }
-        } else if (operator === "minus") {
-            for (let i = 1; i < numArr.length; i++) {
-                result -= Number(numArr[i]);
-            }
-        } else if (numArr.length === 0) {
-            result = "No numbers provided...";
-        } else if (operator !== "plus" && operator !== "minus") {
-            result = `Invalid operation: ${operator}`;
+ function calculator() {
+    
+    let result = 0;
+    if(process.argv.length < 3) {
+     return 'No operation provided...'
+    }
+    else if(!process.argv[3]) {
+     return 'No numbers provided...'
+    }
+    
+    
+    if(process.argv[2] === 'plus') {
+        for(let i = 3; i < process.argv.length; i++) {
+            result += Number(process.argv[i]);
         }
-        return result;
-};
+        return result
+    }
+   
+    if(process.argv[2] === 'minus') {
+    result = process.argv[3]
+        for(let i = 4; i < process.argv.length; i++) {
+            result -= Number(process.argv[i]);
+    }
+        return result
+    } else {
+        return `Invalid operation: ${process.argv[2]}`
+    };
+
+ }
 
 // Don't change anything below this line.
 module.exports = calculator;

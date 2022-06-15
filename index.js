edf0ba40662bb319f1f36a92771be99784401d63
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-06-15 16:32:43 -0400

    passing 8 of 9

--- index.js
+++ index.js
@@ -4,7 +4,33 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    console.log(process.argv)
+    // console.log(process.argv[2])
+    if (process.argv[2] === `plus`){
+        let sum = 0
+    
+        for (let i = 3; i < process.argv.length; i++) {
+            // sum += Number(process.argv[i])
+            sum += parseInt(process.argv[i])
+        }
+        return sum;
+    }else if (process.argv[2] === `minus`){
+        let sum = process.argv[3]
+        for (let j = 4; j < process.argv.length; j++) {
+            sum -= parseInt(process.argv[j]);
+            
+        }
+        return sum;
+} else if (!process.argv[2]){
+    return `No operation provided...`
+}else if(!process.argv[3]){
+    return `No numbers provided...`
+}else if (process.argv[2] !== 'plus' || process.argv[2] !=='minus'){
+    return `Invalid operation: modulo`
+}
+}
+
 
 // Don't change anything below this line.
 module.exports = calculator;

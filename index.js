c5b153bfc4ae79c0f5d8ed03079518f3e4fd061b
NRiddick113 <NatashaRiddick@pursuit.org>
2022-06-15 22:38:10 -0400

    soluation

--- index.js
+++ index.js
@@ -4,7 +4,31 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+  let operator = process.argv[2];
+  let numbers = process.argv.slice(3);
+  let sum = Number(numbers[0])
+  if (!operator){
+ return "No operation provided..."
+} if (!numbers[0]){
+ return "No numbers provided..." 
+} if(operator !== 'plus' && operator !== "minus"){
+ return `Invalid operation: ${operator}` 
+}if(operator === "plus"){
+for (let i = 1; i < numbers.length; i++) {
+    sum += Number(numbers[i])
+}   
+ return sum     
+}if(operator === "minus"){
+    for (let i = 1; i < numbers.length; i++) {
+     sum -= Number(numbers[i])
+} return sum
+}
+
+}
+
+
+
 
 // Don't change anything below this line.
 module.exports = calculator;

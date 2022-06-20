6f44cd0651d69919188cb188aee6edce867ac8fd
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2022-06-20 16:30:46 -0400

    Add code to calculator() which either adds or subtracts numbers passed through as arguments.

--- index.js
+++ index.js
@@ -4,7 +4,32 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    if (!process.argv[2]) {
+        return "No operation provided..."
+    } else {
+        if (!process.argv[3]) {
+            return "No numbers provided..."
+        } else {
+            let sum = 0;
+            switch (process.argv[2]) {
+                case "plus" :
+                    for (i = 3; i < process.argv.length; i++){
+                        sum += Number(process.argv[i]);
+                    }
+                    return sum;
+                case "minus" :
+                    sum = Number(process.argv[3]);  
+                    for (i = 4; i < process.argv.length; i++){
+                        sum -= Number(process.argv[i]);
+                    }
+                    return sum;
+                default : 
+                    return `Invalid operation: ${process.argv[2]}`;
+            }
+        }
+    }
+}
 
 // Don't change anything below this line.
 module.exports = calculator;

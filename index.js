a6038be8f7a72e3a74ae82a6a31f5d02f18d9b2d
MarsiaRochelin <marsrochelin@pursuit.org>
2022-06-24 16:01:00 -0400

    progress

--- index.js
+++ index.js
@@ -4,7 +4,24 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+let operator = process.argv[2];
+let numsArr = process.argv.slice(3).map(Number);
+let sum = 0 ;    
+console.log(numsArr);
+
+if (!operator){
+    return "No operation provided...";
+} else if(!numsArr.length){
+    return "No numbers provided...";
+}  else {
+    return "Invalid operation: modulo";
+}
+};
+
+
+
 
 // Don't change anything below this line.
 module.exports = calculator;
+

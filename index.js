5f55bf40b8bbdbfde8499af2e1b5d9b8c0cd6c20
SasukeDaTony <tonytaylor@pursuit.org>
2022-06-26 07:48:41 -0400

    possible-answer

--- index.js
+++ index.js
@@ -4,7 +4,24 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+
+
+const input = process.argv[2] 
+const totalArgs = []
+let i = 3
+function calculator(input,result) {
+    do {
+        totalArgs.push(process.argv[i])
+        i++ ;
+    } while (i >2 && i < totalArgs.length)
+    if (input === 'plus') {
+        for (let j = 0; j < totalArgs.length ; j++) {
+            result += totalArgs[j];
+            return result ;
+        }
+        
+    } 
+}
 
 // Don't change anything below this line.
 module.exports = calculator;

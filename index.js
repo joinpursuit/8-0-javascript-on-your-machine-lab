e54ac40b76e30edce729377d227e0c78510d5f47
Karyn Huston <huston.karyn@gmail.com>
2022-06-29 20:21:36 -0400

    completed js on your machine lab

--- index.js
+++ index.js
@@ -6,10 +6,46 @@
  */
 function calculator(number) {
 
+const operation = process.argv[2];
+const grabStrings = process.argv.slice(3);
 
 
+const numbersSet = []
 
-    
+for(const string of grabStrings){
+
+    numbersSet.push(Number(string))
+}
+if(!operation){
+    return 'No operation provided...'
+
+} 
+if(!numbersSet.length){
+     return 'No numbers provided...'
+
+} 
+if(operation === 'plus'){
+ 
+    let sum = 0;
+
+for(const number of numbersSet){
+
+    sum += number;
+}
+    return sum
+}
+if(operation === 'minus'){
+
+    let answer = numbersSet[0]
+
+    for(let i=1; i< numbersSet.length; i++){
+
+        answer -= numbersSet[i];
+    }
+    return answer;
+    }
+
+return `Invalid operation: ${operation}`;
 }
 
 // Don't change anything below this line.

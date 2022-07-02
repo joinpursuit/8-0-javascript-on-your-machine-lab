4f549989e2d5da67968e696763853054a1a7bc1f
Tech-Kat <katpelle@pursuit.org>
2022-07-02 19:23:10 -0400

    javascript-machine

--- index.js
+++ index.js
@@ -4,7 +4,44 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() { 
+    const operation = process.argv[2];
+    // program starts with [2] b/c of node nd file location shows up as [0] & [1]
+    const numberStrings = process.argv.slice(3);
+    // starts at posit. 3 that comes after first argument that we 1st input(posit. 2), also takes the #s and puts them into # strings
+    const numbers = [];
+    // converst# string into real numbers (from line 10)
+    for (const string of numberStrings) {
+        // loop that is going through each test
+        numbers.push(Number(string));
+    }
+    if (!operation) {
+        // if there isnt an operation, it does nothing
+        return "No operation provided...";
+    }
+    if (!numbers.length) {
+        // if the #.length is 0 (has no #s) or doesnt exist
+        return "No numbers provided...";
+    }
+    if (operation === "plus") {
+        let sum = 0;
+        for(const number of numbers) {
+            sum += number;
+            // inside sum make it equal to sum + the #. EX sum = sum + number
+        }
+        return sum;
+    }
+    if (operation === "minus") {
+        let answer = numbers[0];
+        // let answer be the first # in the numbers array
+        for(let i =1; i < numbers.length; i++) {
+            answer -= numbers[i];
+        }
+        return answer;
+    }
+    return `Invalid operation: ${operation}`;
+    // default if all conditions are not met
+}
 
 // Don't change anything below this line.
 module.exports = calculator;

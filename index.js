23dd6c7c497ceaeba97d223bbc6a472b6ee95fc7
Keeanu Rodriguez <keeanu7@gmail.com>
2022-10-12 15:12:13 -0400

    first

--- index.js
+++ index.js
@@ -4,7 +4,20 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+const argv2 = process.argv
+const argv = argv2.slice(2)
+const command = argv[0]
+const number1 = parseInt(argv[1])
+const number2 = parseInt(argv[2])
+    if (process.argv[2] === "plus"){
+console.log(number1 + number2)
+    } else if (command === "minus"){
+console.log(number1 - number2)
+    } else {
+        console.log("hi")
+    }
+}
 
 // Don't change anything below this line.
 module.exports = calculator;

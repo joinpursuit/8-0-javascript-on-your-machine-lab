66bb9e903505d44db7943120b83445469483bc9e
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2022-10-13 08:56:52 -0400

    pass error handling test

--- index.js
+++ index.js
@@ -7,6 +7,14 @@
  * depending on the arguments added to the command line.
  */
 function calculator() {
+    if (process.argv.length == 2) {
+        return "No operation provided..."
+    }
+
+    if (process.argv.length == 3) {
+        return "No numbers provided..."
+    }
+
     if (process.argv.length >= 4) {
         let total = 0;
 
@@ -19,11 +27,15 @@ function calculator() {
             for(let i = 4; i < process.argv.length; i++){
                 total -= Number(process.argv[i]);
             }
+        } else {
+            return "Invalid operation: " + process.argv[2]
         }
 
         return total;
     }
 }
+
+calculator()
 
 // Don't change anything below this line.
 module.exports = calculator;

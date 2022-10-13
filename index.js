58b418ada56a8d03c4c706b64c3662608c36421f
Jyoti Singh <jyo.singh88@gmail.com>
2022-10-13 02:15:54 -0400

    2/7checks

--- index.js
+++ index.js
@@ -5,37 +5,18 @@
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
 function calculator() {
-     let resultedNumber  = 0
-    if (process.argv[2]=== undefined){
-        return `No operation provided...`
-    } else if 
-    ((process.argv[2] !== "plus") &&(process.argv[2] !== "minus")){
-return `Invalid operation: ${process.argv[2]}`
-
-    } else if (process.argv[3]=== undefined ){
-        return `No numbers provided...`
+    let result = 0;
+    //error handling//
+    if (process.argv.length < 3) {
+      return 'No operation provided...';
+    } else if (!process.argv[3]) {
+      return 'No numbers provided...';
     }
-    // process.argv[2]=== "plus"? x = "plus": x = "minus";
-    
-   if (process.argv[2] === "plus") { 
-       for (let i=3; i< process.argv.length; i++){
-       
-       
-resultedNumber += Number(process.argv[i]);
-} 
-   return resultedNumber 
-   
-     
-} else if (process.argv[2]=== "minus"){
-    resultedNumber = process.argv[3] 
-    let count = 0 
-    for (let i =4; i < process.argv.length;i++){
-        count += Number(process.argv[i]);
-       console.log(count)
-    }
-    return resultedNumber - count ;
-} console.log(resultedNumber)
-//starting from [3] and subtracting from the number//
 }
-// Don't change anything below this line.
+//addition testing//
+
+
+
+
+
 module.exports = calculator;

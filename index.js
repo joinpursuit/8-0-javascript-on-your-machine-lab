2b98aaf97d4bfb55eca82ccab197cb8ba0fef4c0
aardvarkpepper <aardvarkpepper@gmail.com>
2022-10-16 19:43:06 -0400

    Modify index.js with code that passes the tests

--- index.js
+++ index.js
@@ -4,7 +4,30 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    let tString = process.argv[2];
+    let sumV = 0;
+    if (!tString) {
+        return "No operation provided...";
+    } else if (tString !== "plus" && tString !== "minus"){
+        if (isNaN(parseInt(tString))){
+            return `Invalid operation: ${process.argv[2]}`;
+        }
+    } else if (!process.argv[3]) {
+        return "No numbers provided..."
+    } else if (tString === "plus") {
+        for (let i = 3; i < process.argv.length; i++) {
+            sumV += parseInt(process.argv[i]);
+        }
+        return sumV;
+    } else if (tString === "minus") {
+        sumV = process.argv[3];
+        for (let i = 4; i < process.argv.length; i++) {
+            sumV -= parseInt(process.argv[i]);
+        }
+        return sumV;
+    }
+}
 
 // Don't change anything below this line.
 module.exports = calculator;

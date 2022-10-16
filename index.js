d81e8bc413103bd05a7b2874b5c6fe21acccd325
BertCasale <bthebest55@gmail.com>
2022-10-16 18:19:03 -0400

    calculate

--- index.js
+++ index.js
@@ -7,24 +7,35 @@
 function calculator() {
     //create variable set to first input
     let calculation = process.argv[2];
-    //a variable
-    let number;
+    let result = Number(process.argv[3]);
     //check if theres any errors
     if (calculation === undefined){
+        //no operations error
         return "No operation provided..."
     } else if (calculation !== "plus" && calculation !== "minus"){
+        //invalid operations error
         return `Invalid operation: ${calculation}`
     } else if (process.argv[3] === undefined){
+        //no numbers error
         return "No numbers provided..."
     }
-    
-
-    //the calculated price
-    // let result;
-    // if (calculation === "plus"){
-        
-    // }
+    //go through the input numbers
+    for (i = 4; i < process.argv.length; i++){
+        //convert the strings to numbers and set num variable equal to the value
+        let num = Number(process.argv[i])
+ 
+        //if calculations is plus, add all the numbers to result
+        if (calculation === "plus"){
+            result += num; 
+        //if calculations is minus, subtract all the numbers from result    
+        } else if (calculation === "minus"){
+            result -= num; 
+        }
+    }
+return result;
 }
+
+//ashley montague
 
 // Don't change anything below this line.
 module.exports = calculator;

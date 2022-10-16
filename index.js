9c1ceaaafc32a9d371b9ddf84c522078d37629cc
BertCasale <bthebest55@gmail.com>
2022-10-16 17:49:06 -0400

    error messages

--- index.js
+++ index.js
@@ -4,7 +4,27 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    //create variable set to first input
+    let calculation = process.argv[2];
+    //a variable
+    let number;
+    //check if theres any errors
+    if (calculation === undefined){
+        return "No operation provided..."
+    } else if (calculation !== "plus" && calculation !== "minus"){
+        return `Invalid operation: ${calculation}`
+    } else if (process.argv[3] === undefined){
+        return "No numbers provided..."
+    }
+    
+
+    //the calculated price
+    // let result;
+    // if (calculation === "plus"){
+        
+    // }
+}
 
 // Don't change anything below this line.
 module.exports = calculator;

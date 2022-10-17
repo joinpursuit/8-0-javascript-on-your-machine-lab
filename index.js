c395057aa72fdb46d58e0b2acb11f85d1d756e05
pjungjs <pjungjs@gmail.com>
2022-10-17 11:49:32 -0400

    3rd commit: organized

--- index.js
+++ index.js
@@ -4,54 +4,32 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-//  const command = process.argv[2];
-//  const number = process.argv[3];
- let result = 0;
- 
- function calculator() {
-     if (process.argv[2] === undefined) {
-        return "No operation provided...";
-     } else if (process.argv[3] === undefined) {
-        return "No numbers provided...";
-     } else {
-        let arr = [];
-        for (let i = 3; i < process.argv.length; i++) {
-            arr.push(parseInt(process.argv[i]));
-        }
-        for (let i = 0; i < arr.length; i++) {
-            if (process.argv[2] === 'plus') {
-                result = arr.reduce((a,b) => a + b, 0);
-            } else if (process.argv[2] === 'minus') {
-                result = arr.reduce((a, b) => a - b);
-            } else {
-                return `Invalid operation: ${process.argv[2]}`;
-            }
-        }
-     }
-    return result;
- }
+// const command = process.argv[2];
+// const number = process.argv[3];
+let result = 0;
+function calculator() {
+    if (process.argv[2] === undefined) {
+       return "No operation provided...";
+    } else if (process.argv[3] === undefined) {
+       return "No numbers provided...";
+    } else {
+       let arr = [];
+       for (let i = 3; i < process.argv.length; i++) {
+           arr.push(parseInt(process.argv[i]));
+       }
+       for (let i = 0; i < arr.length; i++) {
+           if (process.argv[2] === 'plus') {
+               result = arr.reduce((a, b) => a + b);
+           } else if (process.argv[2] === 'minus') {
+               result = arr.reduce((a, b) => a - b);
+           } else {
+               return `Invalid operation: ${process.argv[2]}`;
+           }
+       }
+    }
+   return result;
+}
 
-
-
-//  let result = 0;
-//  function calculator() {
-//      if (process.argv[2] === undefined) {
-//          return "No operation provided...";
-//      } else if (process.argv[3] === undefined) {
-//          return "No numbers provided...";
-//      } else {
-//          for (let i = 3; i < process.argv.length; i++) {
-//              if (process.argv[2] === 'plus') {
-//                  result += parseInt(process.argv[i]);
-//              } else if (process.argv[2] === 'minus') {
-//                  result = -parseInt(process.argv[i]) + result;
-//              } else {
-//                  return `Invalid operation: ${process.argv[2]}`;
-//              }
-//          }
-//      }
-//      return result;
-//  }
 
 // Don't change anything below this line.
 module.exports = calculator;

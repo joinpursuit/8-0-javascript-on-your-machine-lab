742b5712256e55f8eed3194d7735295c9f754207
pjungjs <pjungjs@gmail.com>
2022-10-17 10:17:53 -0400

    2nd commit: all 9 tests passed

--- index.js
+++ index.js
@@ -10,22 +10,48 @@
  
  function calculator() {
      if (process.argv[2] === undefined) {
-         return "No operation provided...";
+        return "No operation provided...";
      } else if (process.argv[3] === undefined) {
-         return "No numbers provided...";
+        return "No numbers provided...";
      } else {
-         for (let i = 3; i < process.argv.length; i++) {
-             if (process.argv[2] === 'plus') {
-                 result = parseInt(process.argv[i]) + result;
-             } else if (process.argv[2] === 'minus') {
-                 result = -parseInt(process.argv[i]) + result;
-             } else {
-                 return `Invalid operation: ${process.argv[2]}`;
-             }
-         }
+        let arr = [];
+        for (let i = 3; i < process.argv.length; i++) {
+            arr.push(parseInt(process.argv[i]));
+        }
+        for (let i = 0; i < arr.length; i++) {
+            if (process.argv[2] === 'plus') {
+                result = arr.reduce((a,b) => a + b, 0);
+            } else if (process.argv[2] === 'minus') {
+                result = arr.reduce((a, b) => a - b);
+            } else {
+                return `Invalid operation: ${process.argv[2]}`;
+            }
+        }
      }
-     return result;
+    return result;
  }
+
+
+
+//  let result = 0;
+//  function calculator() {
+//      if (process.argv[2] === undefined) {
+//          return "No operation provided...";
+//      } else if (process.argv[3] === undefined) {
+//          return "No numbers provided...";
+//      } else {
+//          for (let i = 3; i < process.argv.length; i++) {
+//              if (process.argv[2] === 'plus') {
+//                  result += parseInt(process.argv[i]);
+//              } else if (process.argv[2] === 'minus') {
+//                  result = -parseInt(process.argv[i]) + result;
+//              } else {
+//                  return `Invalid operation: ${process.argv[2]}`;
+//              }
+//          }
+//      }
+//      return result;
+//  }
 
 // Don't change anything below this line.
 module.exports = calculator;
